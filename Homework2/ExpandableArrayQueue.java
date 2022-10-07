23e68579182b63ba0014320ee461db07ab447953
christina choi <112435511+cchoi17@users.noreply.github.com>
2022-10-06 21:55:16 -0700

    Update ExpandableArrayQueue.java

--- Homework2/ExpandableArrayQueue.java
+++ Homework2/ExpandableArrayQueue.java
@@ -1,35 +1,33 @@
 import java.util.NoSuchElementException;
 
 public class ExpandableArrayQueue implements Queue {
-    private Object[] q;
+    private static int MINIMUM_CAPACITY = 16;
+    private Object[] elements= new Object[MINIMUM_CAPACITY];
     private int size = 0;
-    private int head = 0;
-    private int tail = 0;
-    private int cap = 16; 
+    private int HEAD = 0;
+    private int TAIL = 0;
 
     public int capacity(){
-        return q.length; 
+        return elements.length; 
     }
+
     public void enqueue(Object item){
-        if(size == 0){
-            q[tail] = item;
-            tail = (tail + 1) % q.length;
-            size++;
+        if(isFull()){
+            changeSize(elements.length * 2);
         }
-        else if(isFull()){
-            doubleSize();
-            throw new NoSuchElementException("Queue is full!");
-        }
+        elements[TAIL] = item;
+        TAIL++;
+        size++;
     }
 
     public Object dequeue(){
-        if (isEmpty()) {
-            throw new NoSuchElementException("Cannot remove from empty queue");
-        }
-        var item = q[head];
-        q[head] = null;
-        head = (head + 1) % q.length;
+        var item = peek();
+        elements[HEAD] = null;
+        ++HEAD;
         size--;
+        if((size() > MINIMUM_CAPACITY) && (size() < (0.25 * elements.length))) {
+            changeSize(elements.length* (1/2)); 
+        }
         return item;
     }
 
@@ -37,23 +35,24 @@ public class ExpandableArrayQueue implements Queue {
         if (isEmpty()) {
             throw new NoSuchElementException("the stack is empty!");
         }
-        return q[size - 1];
+        return elements[HEAD];
     }
 
-    public void doubleSize(){
-
-    }
-
-    public void halfSize(){
-
+    public void changeSize(int newSize){
+        Object[] elements2 = new Object[newSize]; 
+        System.arraycopy(elements, HEAD, elements2, 0, size);
+        elements = elements2; 
+        TAIL -= HEAD; 
+        HEAD=0; 
     }
 
     public boolean isFull(){
-        return size == q.length; 
+        return size == elements.length; 
     }
 
     @Override
     public boolean isEmpty(){
         return size == 0; 
     }
+    public int size(){return size;}
 }
